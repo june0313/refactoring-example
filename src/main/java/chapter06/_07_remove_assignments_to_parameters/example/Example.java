6bebbe5c04986929c549386c70dc772cb711ce3f
Young Jun Park <june0313@gmail.com>
2017-09-04 00:23:21 +0900

    [chapter 06] Remove Assignments to Parameters 예제 작성

---
+++ src/main/java/chapter06/_07_remove_assignments_to_parameters/example/Example.java
@@ -0,0 +1,20 @@
+package chapter06._07_remove_assignments_to_parameters.example;
+
+/**
+ * Created by wayne on 2017. 9. 4..
+ * <p>
+ * Remove Assignments to Parameters (매개변수로의 값 대입 제거)
+ * <p>
+ * 매개변수로 값을 대입하는 코드가 있을 땐
+ * - 매개변수 대신 임시변수를 사용하게 수정하자.
+ */
+public class Example {
+
+    int discount(int inputVal, int quantity, int yearToDate) {
+        if (inputVal > 50) inputVal -= 2;
+        if (quantity > 100) inputVal -= 1;
+        if (yearToDate > 10000) inputVal -= 4;
+        return inputVal;
+    }
+
+}
