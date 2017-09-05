3385c6d506fba27b8ef25ed9a29ed797e4183747
Young Jun Park <june0313@gmail.com>
2017-09-05 22:57:07 +0900

    외상액 계산 코드를 메서드로 추출

--- src/main/java/chapter06/_01_extract_method/practice/Practice.java
+++ src/main/java/chapter06/_01_extract_method/practice/Practice.java
@@ -16,19 +16,21 @@ public class Practice {
     private String _name;
 
     void printOwing() {
-        Enumeration e = _orders.elements();
-        double outstanding = 0.0;
-
         printBanner();
+        double outstanding = getOutstanding();
+        printDetails(outstanding);
+    }
 
+    private double getOutstanding() {
+        Enumeration e = _orders.elements();
+        double result = 0.0;
 
-        // 외상액 계산
         while (e.hasMoreElements()) {
             Order each = (Order) e.nextElement();
-            outstanding = each.getAmount();
+            result = each.getAmount();
         }
 
-        printDetails(outstanding);
+        return result;
     }
 
     private void printDetails(double outstanding) {
