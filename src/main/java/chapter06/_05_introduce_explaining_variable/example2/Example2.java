e8cba20d56a269fed8c156ca16fe54ad8fdde6b8
Young Jun Park <june0313@gmail.com>
2017-09-06 21:52:56 +0900

    [introduce explaining variable] 수량 * 단가 수식을 임시변수로 변경

--- src/main/java/chapter06/_05_introduce_explaining_variable/example2/Example2.java
+++ src/main/java/chapter06/_05_introduce_explaining_variable/example2/Example2.java
@@ -16,8 +16,9 @@ public class Example2 {
     double price() {
         // 결재액(price) = 총 구매액(base price) -
         // 대량 구매 할인(quantity discount) + 배송비(shipping)
-        return _quantity * _itemPrice -
+        final double basePrice = _quantity * _itemPrice;
+        return basePrice -
                 Math.max(0, _quantity - 500) * _itemPrice * 0.05 +
-                Math.min(_quantity * _itemPrice * 0.1, 100.0);
+                Math.min(basePrice * 0.1, 100.0);
     }
 }
