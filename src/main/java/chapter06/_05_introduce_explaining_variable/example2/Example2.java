ef52d997f8e5dd2bec8771ccb1d79252207e5977
Young Jun Park <june0313@gmail.com>
2017-09-06 21:57:24 +0900

    [introduce explaining variable] 주석 제거

--- src/main/java/chapter06/_05_introduce_explaining_variable/example2/Example2.java
+++ src/main/java/chapter06/_05_introduce_explaining_variable/example2/Example2.java
@@ -14,8 +14,6 @@ public class Example2 {
     private double _itemPrice;
 
     double price() {
-        // 결재액(price) = 총 구매액(base price) -
-        // 대량 구매 할인(quantity discount) + 배송비(shipping)
         final double basePrice = _quantity * _itemPrice;
         final double quantityDiscount = Math.max(0, _quantity - 500) * _itemPrice * 0.05;
         final double shipping = Math.min(basePrice * 0.1, 100.0);
