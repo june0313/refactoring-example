6faf739484dcf699a930368140ac43a1219aa475
Young Jun Park <june0313@gmail.com>
2017-09-06 21:59:54 +0900

    [introduce explaining variable] 임시변수를 메서드로 추출

--- src/main/java/chapter06/_05_introduce_explaining_variable/example2/Example2.java
+++ src/main/java/chapter06/_05_introduce_explaining_variable/example2/Example2.java
@@ -14,9 +14,18 @@ public class Example2 {
     private double _itemPrice;
 
     double price() {
-        final double basePrice = _quantity * _itemPrice;
-        final double quantityDiscount = Math.max(0, _quantity - 500) * _itemPrice * 0.05;
-        final double shipping = Math.min(basePrice * 0.1, 100.0);
-        return basePrice - quantityDiscount + shipping;
+        return basePrice() - quantityDiscount() + shipping();
+    }
+
+    private double shipping() {
+        return Math.min(basePrice() * 0.1, 100.0);
+    }
+
+    private double quantityDiscount() {
+        return Math.max(0, _quantity - 500) * _itemPrice * 0.05;
+    }
+
+    private double basePrice() {
+        return _quantity * _itemPrice;
     }
 }
