766798a4b06411cc1a470886f4cc39c77d8c0dd9
Young Jun Park <june0313@gmail.com>
2017-09-09 00:11:40 +0900

    [extract class] Person 클래스에 TelephoneNumber 클래스 필드 작성

--- src/main/java/chapter07/_03_extract_class/example/Person.java
+++ src/main/java/chapter07/_03_extract_class/example/Person.java
@@ -10,6 +10,7 @@ public class Person {
     private String _name;
     private String _officeAreaCode;
     private String _officeNumber;
+    private TelephoneNumber _officeTelephone = new TelephoneNumber();
 
     public String getName() {
         return _name;
