071975be545543bd44610d5eb0aff9c756a74ffa
Young Jun Park <june0313@gmail.com>
2017-09-10 00:10:14 +0900

    [chapter07] Inline Class 예제 작성

---
+++ src/main/java/chapter07/_04_inline_class/example/Person.java
@@ -0,0 +1,24 @@
+package chapter07._04_inline_class.example;
+
+/**
+ * Inline Class (클래스 내용 직접 삽입)
+ * <p>
+ * 클래스의 기능이 너무 적을 땐
+ * - 그 클래스의 모든 기능을 다른 클래스로 합쳐 넣고 원래의 클래스는 삭제하자.
+ */
+public class Person {
+    private String _name;
+    private TelephoneNumber _officeTelephone = new TelephoneNumber();
+
+    public String getName() {
+        return _name;
+    }
+
+    public String getTelephoneNumber() {
+        return _officeTelephone.getTelephoneNumber();
+    }
+
+    public TelephoneNumber getOfficeTelephone() {
+        return _officeTelephone;
+    }
+}
---
+++ src/main/java/chapter07/_04_inline_class/example/TelephoneNumber.java
@@ -0,0 +1,32 @@
+package chapter07._04_inline_class.example;
+
+/**
+ * Inline Class (클래스 내용 직접 삽입)
+ * <p>
+ * 클래스의 기능이 너무 적을 땐
+ * - 그 클래스의 모든 기능을 다른 클래스로 합쳐 넣고 원래의 클래스는 삭제하자.
+ */
+public class TelephoneNumber {
+    private String _areaCode;
+    private String _number;
+
+    public String getAreaCode() {
+        return _areaCode;
+    }
+
+    public void setAreaCode(String _areaCode) {
+        this._areaCode = _areaCode;
+    }
+
+    public String getTelephoneNumber() {
+        return "(" + _areaCode + ") " + _number;
+    }
+
+    public String getNumber() {
+        return _number;
+    }
+
+    public void setNumber(String number) {
+        this._number = number;
+    }
+}
