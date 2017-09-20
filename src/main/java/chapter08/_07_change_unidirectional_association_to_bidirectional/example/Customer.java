434bc0175f39455dfcdfd77ed7a50ac1b4fd41d0
Young Jun Park <june0313@gmail.com>
2017-09-20 22:33:57 +0900

    [chapter08] Change Unidirectional Association to Bidirectional 예제 작성

---
+++ src/main/java/chapter08/_07_change_unidirectional_association_to_bidirectional/example/Customer.java
@@ -0,0 +1,4 @@
+package chapter08._07_change_unidirectional_association_to_bidirectional.example;
+
+public class Customer {
+}
---
+++ src/main/java/chapter08/_07_change_unidirectional_association_to_bidirectional/example/Order.java
@@ -0,0 +1,19 @@
+package chapter08._07_change_unidirectional_association_to_bidirectional.example;
+
+/**
+ * Change Unidirectional Association to Bidirectional (클래스의 단방향 연결을 양방향으로 전환)
+ * <p>
+ * 두 클래스가 서로의 기능을 사용해야 하는데 한 방향으로만 연결되어 있을 땐
+ * - 역 포인터(참조)를 추가하고 두 클래스를 모두 업데이트 할 수 있게 접근 한정자를 수정하자.
+ */
+public class Order {
+    Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
