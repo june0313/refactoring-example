1bf71c8f67c8d0b8b66bc4f52b51e8b4853ec8a3
Young Jun Park <june0313@gmail.com>
2017-09-28 22:15:25 +0900

    [chapter08] Replace Record with Data Class 예제 작성

---
+++ src/main/java/chapter08/_12_replace_record_with_data_class/example/Record.java
@@ -0,0 +1,10 @@
+package chapter08._12_replace_record_with_data_class.example;
+
+/**
+ * Replace Record with Data Class (레코드를 데이터 클래스로 전환)
+ * <p>
+ * 전통적인 프로그래밍 환경에서 레코드 구조를 이용한 인터페이스를 제공해야할 땐
+ * - 레코드 구조를 저장할 덤(Dumb) 데이터 객체를 작성하자.
+ */
+public class Record {
+}
