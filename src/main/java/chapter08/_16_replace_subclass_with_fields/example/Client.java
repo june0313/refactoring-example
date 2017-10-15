a54895b87dc8fa1d95e40207588c1896ef2bfb13
Young Jun Park <june0313@gmail.com>
2017-10-15 12:59:19 +0900

    [Replace Subclass with Fields] Person 하위 클래스를 제거하고 필드와 팩토리 메서드로 전환

--- src/main/java/chapter08/_16_replace_subclass_with_fields/example/Client.java
+++ src/main/java/chapter08/_16_replace_subclass_with_fields/example/Client.java
@@ -2,11 +2,11 @@ package chapter08._16_replace_subclass_with_fields.example;
 
 public class Client {
     public static void main(String[] args) {
-        Person jun = new Male();
+        Person jun = Person.createMale();
         System.out.println("jun is male ? " + jun.isMale());
         System.out.println("jun's code : " + jun.getCode());
 
-        Person sun = new Female();
+        Person sun = Person.createFemale();
         System.out.println("sun is male ? " + sun.isMale());
         System.out.println("sun's code : " + sun.getCode());
     }
