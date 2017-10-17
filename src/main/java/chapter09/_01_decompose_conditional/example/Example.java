7c1f77451885a0b4786b7df2e1bf780e65230914
Young Jun Park <june0313@gmail.com>
2017-10-18 00:10:31 +0900

    [chapter09] Decompose Conditional 예제 작성

---
+++ src/main/java/chapter09/_01_decompose_conditional/example/Example.java
@@ -0,0 +1,28 @@
+package chapter09._01_decompose_conditional.example;
+
+import java.util.Date;
+
+/**
+ * Decompose Conditional (조건문 쪼개기)
+ * <p>
+ * 복잡한 조건문(if-then-else)이 있을 땐
+ * - if, then, else 부분을 각각 메서드로 빼내자
+ */
+public class Example {
+    private static final Date SUMMER_START = new Date();
+    private static final Date SUMMER_END = new Date();
+
+    private int winterRate;
+    private int summerRate;
+    private int winterServiceCharge;
+
+    void someMethod(Date date, int quantity) {
+        int charge;
+
+        if (date.before(SUMMER_START) || date.after(SUMMER_END)) {
+            charge = quantity * winterRate + winterServiceCharge;
+        } else {
+            charge = quantity * summerRate;
+        }
+    }
+}
