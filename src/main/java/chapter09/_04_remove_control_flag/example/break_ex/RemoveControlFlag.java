9a17c58fab007a00c538b0bfcb1114fc1661caea
Young Jun Park <june0313@gmail.com>
2017-10-22 21:23:40 +0900

    [chapter09] Remove Control Flag 예제 작성

---
+++ src/main/java/chapter09/_04_remove_control_flag/example/break_ex/RemoveControlFlag.java
@@ -0,0 +1,30 @@
+package chapter09._04_remove_control_flag.example.break_ex;
+
+/**
+ * Remove Control Flag (제어 플래그 제거)
+ * <p>
+ * 논리 연산식의 제어 플래그 역할을 하는 변수가 있을 땐
+ * - 그 변수를 break문이나 return문으로 바꾸자
+ */
+public class RemoveControlFlag {
+    void checkSecurity(String[] people) {
+        boolean found = false;
+
+        for (String aPeople : people) {
+            if (!found) {
+                if (aPeople.equals("Don")) {
+                    sendAlert();
+                    found = true;
+                }
+                if (aPeople.equals("John")) {
+                    sendAlert();
+                    found = true;
+                }
+            }
+        }
+    }
+
+    private void sendAlert() {
+        System.out.println("alert");
+    }
+}
