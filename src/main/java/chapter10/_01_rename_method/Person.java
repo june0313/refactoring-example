55d6462fe80470420f583caa93561771b8f9720a
Young Jun Park <june0313@gmail.com>
2017-11-18 18:25:43 +0900

    [chapter10] Rename method 예제 작성

---
+++ src/main/java/chapter10/_01_rename_method/Person.java
@@ -0,0 +1,16 @@
+package chapter10._01_rename_method;
+
+/**
+ * Rename Method (메서드명 변경)
+ * <p>
+ * 메서드명을 봐도 기능을 할 수 없을 땐
+ * - 메서드명을 직관적인 이름으로 바꾸자.
+ */
+public class Person {
+    private String _officeAreaCode;
+    private int _officeNumber;
+
+    public String getTelephoneNumber() {
+        return ("(" + _officeAreaCode + ")" + _officeNumber);
+    }
+}
