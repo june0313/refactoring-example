5b8db49cf2c0a7a19572eebf8d5d5f3f628a92c4
Young Jun Park <june0313@gmail.com>
2017-11-18 18:29:48 +0900

    [chapter10] 원본 메서드 호출 코드를 새 메서드 호출하도록 수정

--- src/main/java/chapter10/_01_rename_method/practice/Main.java
+++ src/main/java/chapter10/_01_rename_method/practice/Main.java
@@ -3,6 +3,6 @@ package chapter10._01_rename_method.practice;
 public class Main {
     public static void main(String[] args) {
         Person person = new Person();
-        System.out.println(person.getTelephoneNumber());
+        System.out.println(person.getOfficeTelephoneNumber());
     }
 }
