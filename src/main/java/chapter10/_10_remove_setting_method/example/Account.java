806a5b224afea6a66acb88545abee16487aa331e
Young Jun Park <june0313@gmail.com>
2017-12-14 00:35:33 +0900

    [chapter10] Remove Setting Method 예제 작성

---
+++ src/main/java/chapter10/_10_remove_setting_method/example/Account.java
@@ -0,0 +1,19 @@
+package chapter10._10_remove_setting_method.example;
+
+/**
+ * Remove Setting Method (쓰기 메서드 제거)
+ * <p>
+ * 생성할 때 지정한 필드 값이 절대로 변경되지 말아야 할 땐
+ * - 그 필드를 설정하는 모든 쓰기 메서드를 삭제하자
+ */
+public class Account {
+    private String id;
+
+    public Account(String id) {
+        setId(id);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
