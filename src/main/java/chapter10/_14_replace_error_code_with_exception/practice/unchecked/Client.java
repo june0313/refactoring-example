5cbacf4805c5d722ef310784a4bc3a32f1451c65
Young Jun Park <june0313@gmail.com>
2017-12-25 23:44:01 +0900

    [chapter10] Replace Error Code with Exception 예제 작성
    - unchecked 예외에 대한 예제

---
+++ src/main/java/chapter10/_14_replace_error_code_with_exception/practice/unchecked/Client.java
@@ -0,0 +1,23 @@
+package chapter10._14_replace_error_code_with_exception.practice.unchecked;
+
+public class Client {
+    public static void main(String[] args) {
+        Account account = new Account();
+
+        int amount = 100;
+
+        if (account.withdraw(amount) == -1) {
+            handleOverdrawn();
+        } else {
+            doTheUsualThing();
+        }
+    }
+
+    private static void doTheUsualThing() {
+
+    }
+
+    private static void handleOverdrawn() {
+
+    }
+}
