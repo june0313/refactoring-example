eba6ca9726d2ecf3f42e8bf0a84d2e423e70bbc1
Young Jun Park <june0313@gmail.com>
2017-12-29 23:38:24 +0900

    [chapter10] Replace Exception with Test 예제 작성

---
+++ src/main/java/chapter10/_15_replace_exception_with_test/example/Resource.java
@@ -0,0 +1,4 @@
+package chapter10._15_replace_exception_with_test.example;
+
+public class Resource {
+}
---
+++ src/main/java/chapter10/_15_replace_exception_with_test/practice/ResourcePool.java
@@ -0,0 +1,28 @@
+package chapter10._15_replace_exception_with_test.practice;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/**
+ * Replace Exception with Test 예외 처리를 테스트로 교체
+ * <p>
+ * 호출 부분에 사전 검사 코드를 넣으면 될 상황인데 예외 통지를 사용할 땐
+ * - 호출 부분이 사전 검사를 실시하게 수정하자.
+ */
+public class ResourcePool {
+    private Stack<Resource> available;
+    private Stack<Resource> allocated;
+
+    Resource getResource() {
+        Resource result;
+        try {
+            result = available.pop();
+            allocated.push(result);
+            return result;
+        } catch (EmptyStackException e) {
+            result = new Resource();
+            allocated.push(result);
+            return result;
+        }
+    }
+}
