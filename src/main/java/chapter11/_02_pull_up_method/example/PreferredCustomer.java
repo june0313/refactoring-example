2930db7f732f3426d4ff6c3fc29c745df1a811ab
Young Jun Park <june0313@gmail.com>
2018-01-01 23:19:18 +0900

    [chapter11] Pull Up Method 예제 작성

---
+++ src/main/java/chapter11/_02_pull_up_method/example/PreferredCustomer.java
@@ -0,0 +1,14 @@
+package chapter11._02_pull_up_method.example;
+
+import java.util.Date;
+
+public class PreferredCustomer extends Customer {
+    void createBill(Date date) {
+        double changeAmount = changeFor(lastBillDate, date);
+        addBill(date, changeAmount);
+    }
+
+    private double changeFor(Date lastBillDate, Date date) {
+        return 1;
+    }
+}
---
+++ src/main/java/chapter11/_02_pull_up_method/practice/Customer.java
@@ -0,0 +1,18 @@
+package chapter11._02_pull_up_method.practice;
+
+import java.util.Date;
+
+/**
+ * Pull Up Method (메서드 상향)
+ * <p>
+ * 기능이 같은 메서드가 여러 하위 클래스에 들어 있을 땐
+ * - 그 메서드를 상위 클래스로 옮기자.
+ */
+public class Customer {
+    Date lastBillDate;
+
+    void addBill(Date date, double amount) {
+        // do something
+    }
+
+}
