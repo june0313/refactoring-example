394b4bce74824348e2f21a13b54d1b5b61a59259
Young Jun Park <june0313@gmail.com>
2018-01-13 18:19:05 +0900

    [chapter11] Extract Superclass 예제 작성

---
+++ src/main/java/chapter11/_07_extract_superclass/example/Department.java
@@ -0,0 +1,48 @@
+package chapter11._07_extract_superclass.example;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+/**
+ * Extract Superclass (상위 클래스 추출)
+ * <p>
+ * 기능이 비슷한 두 클래스가 있을 땐
+ * - 상위클래스를 작성하고 공통된 기능들을 그 상위 클래스로 옮기자.
+ */
+public class Department {
+    private String name;
+    private Vector<Employee> staff = new Vector<>();
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalAnnualCost() {
+        Enumeration<Employee> e = getStaff();
+        int result = 0;
+
+        while (e.hasMoreElements()) {
+            Employee each = e.nextElement();
+            result += each.getAnnualCoast();
+        }
+
+        return result;
+    }
+
+    public int getHeadCount() {
+        return staff.size();
+    }
+
+    private Enumeration<Employee> getStaff() {
+        return staff.elements();
+    }
+
+    public void addStaff(Employee arg) {
+        staff.addElement(arg);
+    }
+
+}
---
+++ src/main/java/chapter11/_07_extract_superclass/practice/Employee.java
@@ -0,0 +1,25 @@
+package chapter11._07_extract_superclass.practice;
+
+public class Employee {
+    private String name;
+    private int annualCoast;
+    private String id;
+
+    public Employee(String name, int annualCoast, String id) {
+        this.name = name;
+        this.annualCoast = annualCoast;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAnnualCoast() {
+        return annualCoast;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
