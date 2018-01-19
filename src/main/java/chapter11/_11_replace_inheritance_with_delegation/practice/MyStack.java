dd8ef1efc76695df37223b662fa6c9152fd263ea
Young Jun Park <june0313@gmail.com>
2018-01-19 23:39:25 +0900

    [chapter11] Vector 상속 구조를 끊고 위임으로 전환

--- src/main/java/chapter11/_11_replace_inheritance_with_delegation/practice/MyStack.java
+++ src/main/java/chapter11/_11_replace_inheritance_with_delegation/practice/MyStack.java
@@ -8,14 +8,24 @@ import java.util.Vector;
  * 하위클래스가 상위클래스 인터페이스의 일부만 사용하거나 데이터를 상속받지 않게 해야 할 땐
  * - 상위클래스를 참조하는 필드를 만들고, 모든 메서드가 그 상위클래스에 위임하도록 수정한 후 상속 구조를 제거하자.
  */
-public class MyStack<T> extends Vector<T> {
+public class MyStack<T> {
+    private Vector<T> vector = new Vector<>();
+
     public void push(T element) {
-        insertElementAt(element, 0);
+        vector.insertElementAt(element, 0);
     }
 
     public T pop() {
-        T result = firstElement();
-        removeElementAt(0);
+        T result = vector.firstElement();
+        vector.removeElementAt(0);
         return result;
+    }
+
+    public int size() {
+        return vector.size();
+    }
+
+    public boolean isEmpty() {
+        return vector.isEmpty();
     }
 }
