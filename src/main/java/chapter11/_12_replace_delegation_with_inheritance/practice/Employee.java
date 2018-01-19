d72b7f8f6399b8d07741d49c97fde1867e885b80
Young Jun Park <june0313@gmail.com>
2018-01-20 01:15:47 +0900

    [chapter11] 대리 객체를 상속 관계로 변경

--- src/main/java/chapter11/_12_replace_delegation_with_inheritance/practice/Employee.java
+++ src/main/java/chapter11/_12_replace_delegation_with_inheritance/practice/Employee.java
@@ -6,19 +6,9 @@ package chapter11._12_replace_delegation_with_inheritance.practice;
  * 위임을 이용 중인데 인터페이스 전반이 간단한 위임으로 도배될 땐
  * - 위임 클래스를 대리 객체의 하위 클래스로 만들자.
  */
-public class Employee {
-    private Person person = new Person();
-
-    public String getName() {
-        return person.getName();
-    }
-
-    public void setName(String name) {
-        person.setName(name);
-    }
-
+public class Employee extends Person {
     @Override
     public String toString() {
-        return "사원 : " + person.getLastName();
+        return "사원 : " + getLastName();
     }
 }
