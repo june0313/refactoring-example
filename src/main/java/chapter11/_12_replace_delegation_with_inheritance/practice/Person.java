ebfe4445618398a4487e811084e0780cabbed06b
Young Jun Park <june0313@gmail.com>
2018-01-20 01:13:37 +0900

    [chapter11] Replace Delegate with Inheritance 예제 작성

---
+++ src/main/java/chapter11/_12_replace_delegation_with_inheritance/practice/Person.java
@@ -0,0 +1,17 @@
+package chapter11._12_replace_delegation_with_inheritance.practice;
+
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return name.substring(name.lastIndexOf(' ') + 1);
+    }
+}
